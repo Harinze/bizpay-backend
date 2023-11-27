e348ceb1ef15beaa0df00b86500e71759e0caba4
Kingsley Ibe <kingsley.ibe@decagon.dev>
2023-11-27 08:57:10 +0100

    Added build

--- dist/app.js
+++ dist/app.js
@@ -17,6 +17,30 @@ app.use(body_parser_1.default.json());
 app.use('/signup', index_1.default);
 app.use('/login', index_1.default);
 app.use('/createclientprofile', index_1.default);
+app.get('/', (req, res) => {
+    res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Welcome to Bizpay</title>
+        <style>
+          body {
+            font-family: 'Arial', sans-serif;
+            background-color: lightgreen;
+            text-align: center;
+            padding: 50px;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>Welcome to Bizpay backend application</h1>
+        <p>This is a simple message to indicate that the server is running.</p>
+      </body>
+    </html>
+  `);
+});
 app.listen(port, () => {
     console.log(`Server is Ok`);
 });
